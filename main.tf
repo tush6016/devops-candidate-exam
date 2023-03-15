2a71d77d2a558db2768a2132c91ec1aaeb97e1a2
Tushar K <tushars6016@gmail.com>
2023-03-15 15:54:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ resource "aws_vpc" "vpc" {
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_file = "lambda_function.py"
+  source_file = "./lambda_function.py"
   output_path = "lambda_function.zip"
 }
 

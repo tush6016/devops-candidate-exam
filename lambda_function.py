bb30c323c8fdaf916dfbd15e50c6bc581576e9c9
Tushar K <tushars6016@gmail.com>
2023-03-16 20:54:57 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,27 +1,13 @@
-import json
-import boto3
-import requests
-
-url = 'https://2xfhzfbt31.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data'
-headers = {'X-Siemens-Auth': 'test'}
-
-payload = {
-    "subnet_id": "aws_subnet.private-subnet.id",
-    "name": "Tushar_Kamble",
-    "email": "tushars6016@gmail.com"
-}
-
-payload_json = json.dumps(payload)
-
-region = boto3.Session().eu-west-1a
-
-response = requests.post(url, headers=headers, data=payload_json)
-
-response = requests.post(url, headers=headers, json=payload)
-if response.status_code == 200:
-    print("Success! Status code: ", response.status_code)
-else:
-    print("Error! Status code: ", response.status_code)
-
-print(response.text)
+  import json
+  import requests
+   
+  def lambda_handler(event, context):
+    endpoint = event['endpoint']
+    auth_header = event['auth_header']
+    payload = event['payload']
+    response = requests.post(endpoint, headers=eval(auth_header), data=payload)
+    return {
+        'statusCode': response.status_code,
+        'body': response.text
+    }
 

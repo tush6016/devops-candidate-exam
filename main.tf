94b40230babd3dc5bdbab9d37cb54ab3df672b48
Tushar K <tushars6016@gmail.com>
2023-03-16 23:12:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,101 +1,80 @@
-resource "aws_internet_gateway" "ig" {
-    vpc_id = data.aws_vpc.vpc
-
-    tags = {
-        Name = "IG"
-    }
-  
-}
-
-resource "aws_route_table" "route1" {
-    vpc_id = data.aws_vpc.vpc
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.ig
-    }
-
-    route {
-        ipv6_cidr_block = "::/0"
-        gateway_id = aws_internet_gateway.ig
-    }
-
-    tags = {
-      Name = "Route-table"
-    }
-
-}
-
-resource "aws_subnet" "private-subnet" {
-  vpc_id = data.aws_vpc.vpc
+resource "aws_subnet" "private_subnet" {
+  vpc_id = data.aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone = eu-west-1a
+  availability_zone = "eu-west-1a"
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "private-subnet"
+    Name = "Private Subnet"
   }
 }
 
 
-resource "aws_route_table_association" "rt-sub" {
-    subnet_id = aws_subnet.private-subnet
-    route_table_id = aws_route_table.route1
-  
+resource "aws_route_table" "private_rt" {
+  vpc_id = data.aws_vpc.vpc.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = data.aws_nat_gateway.nat.id
+  }
 }
 
+resource "aws_route_table_association" "private_rta" {
+  subnet_id = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
 
 provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_lambda_function" "api_lambda" {
-  filename         = "lambda_function.zip"
-  function_name    = "api_lambda_function"
-  role             = data.aws_iam_role.lambda
-  handler          = "lambda_function.lambda_handler"
-  runtime          = "python3.9"
-  timeout          = 60
-  memory_size      = 128
-  subnet_ids       = [aws_subnet.private_subnet.id]
-  security_group_ids = [aws_security_group.lambda_sg.id]
+resource "aws_lambda_function" "example_lambda" {
+  filename      = "lambda.zip"
+  function_name = "lambda_function"
+  role          = data.aws_iam_role.lamda
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+  timeout       = 300
+
+  vpc_config {
+    subnet_ids         = aws_subnet.private_subnet.id
+    security_group_ids = aws_security_group.lambda_sg.id
+  }
+
   environment {
     variables = {
-      SUBNET_ID = aws_subnet.private_subnet.id
-      NAME = "Tushar Kamble"
-      EMAIL = "tushars6016@gmail.com"
+      api_endpoint = "https://2xfhzfbt31.execute-api.eu-west-1.amazonaws.com/candidate-email_serverless_lambda_stage/data"
+      auth_header  = "test"
+      name         = Tushar_Kamble
+      email        = tushars6016@gmail.com
+      subnet_id    = aws_subnet.private_subnet.id
     }
   }
+
+  depends_on = [
+    aws_security_group.lambda_sg
+  ]
 }
+
 
 
 resource "aws_security_group" "lambda_sg" {
-  name_prefix = "lambda_sg"
-  vpc_id      = data.aws_vpc.vpc
+  name_prefix = "lambda_sg_"
+  vpc_id      = var.vpc_id
 
   ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.vpc.cidr_block]
+    protocol = "tcp"
+    from_port = 0
+    to_port   = 65535
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    protocol = "tcp"
+    from_port = 0
+    to_port   = 65535
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = data.aws_vpc.vpc
-  cidr_block = "10.0.1.0/24"
-}
-
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
 }
 
 data "archive_file" "lambda_zip" {
@@ -111,6 +90,4 @@ locals {
     email = "tushars6016@gmail.com"
   })
 }
-
-
 
